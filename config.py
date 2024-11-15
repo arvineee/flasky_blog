60ba1474f19d7d2e0661afbd18fd4098b2b77e73
Arvineee <kiruifelix03@gmail.com>
2024-11-16 00:15:07 +0300

    update on secretkey

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')  
+    SECRET_KEY = os.urandom(16) 
     
     # Database URI
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL')  #
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     
     # Upload folder for storing files
@@ -17,6 +17,6 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', MAIL_USERNAME)  
+    MAIL_USERNAME = 'kiruifelix03@gmail.com'
+    MAIL_PASSWORD = 'rjdzvecxkiitmtco'
+    MAIL_DEFAULT_SENDER = 'kiruifelix03@gmail.com'
