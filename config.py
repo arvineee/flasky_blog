e149c997a2a8d8b87a54fd397b656cb879931698
Arvineee <kiruifelix03@gmail.com>
2024-12-18 13:48:36 +0300

    pastgress db used

--- config.py
+++ config.py
@@ -1,14 +1,22 @@
 import os
+import psycopg2
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.urandom(16) 
-    
+
     # Database URI
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI =  'postgresql://arvine:qcMUZd5KPu99si45RXw7RaetkO5BAINb@dpg-ctha3b0gph6c73dad1n0-a.oregon-postgres.render.com/arval'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     
+    conn = psycopg2.connect(
+    dbname="arval",
+    user="arvine",
+    password="qcMUZd5KPu99si45RXw7RaetkO5BAINb",
+    host="dpg-ctha3b0gph6c73dad1n0-a.oregon-postgres.render.com"
+)
+
     # Upload folder for storing files
     UPLOAD_FOLDER = os.path.join(base_dir, 'app/static/images')
     
--- run.py
+++ run.py
@@ -8,4 +8,4 @@ load_dotenv()
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
