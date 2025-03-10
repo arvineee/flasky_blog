91bce666c1421cd99edbc375939577e0d22c9e47
arvineee <76680607+arvineee@users.noreply.github.com>
2025-03-10 05:28:39 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -26,6 +26,7 @@ class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True, index=True)
     title = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
     desc = db.Column(db.Text, nullable=False)
     date_pub = db.Column(db.String(), default=date.today)
     image_url = db.Column(db.String(), nullable=True)
