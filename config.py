55603fc7d45114712566ace559394757b1713b4a
arvineee <kiruifelix03@gmail.com>
2025-05-29 19:14:16 +0300

    env

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 import os
-base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-key")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    UPLOAD_FOLDER = os.path.join(base_dir, 'app/static/images')
-    VIDEO_UPLOAD_FOLDER = os.path.join(base_dir, 'app/static/videos')
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
